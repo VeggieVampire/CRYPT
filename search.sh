6f47aadf1437fefeb3daee0396f36cf1fc9b0e64
Nick Farrow <VeggieVampire@users.noreply.github.com>
2018-12-12 02:39:46 -0600

    Create search.sh

---
+++ search.sh
@@ -0,0 +1,39 @@
+#!/bin/ksh
+#  *****Z
+#
+##########################
+# Author Nick Farrow     #
+# Initial Release: 0     #
+##########################
+RED='\033[0;31m'        #RED
+GREEN='\033[0;32m'      #GREEN
+YELLOW='\033[0;33m'     #YELLOW
+NC='\033[0m'    #No Color
+
+
+echo "What IP are you looking for?"
+#read SearchIP
+SearchIP="111.222.333.444"
+
+if [ -z "$SearchIP" ]
+then
+  echo "Missing  IP!!!!!!!!"
+  echo "missing file name"
+  exit 1
+else
+  printf "Looking for $SearchIP\n"
+  date
+fi
+
+echo "clearing local IP list"
+cat /dev/null>./LocalIPlist
+IFS=$'\n'       # make newlines the only separator
+for x in $(cat ./AllDevices)
+do
+DeviceIP=$(echo $x|awk '{print $1}')
+DeviceName=$(echo $x|awk '{print $2}')
+DeviceType=$(echo $x|awk '{print $3$4$5$6$7}')
+echo "$DeviceName using $DeviceIP is a $DeviceType"
+./send.sh $DeviceIP $DeviceType
+
+done
